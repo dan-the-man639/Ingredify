73691ba0b56c4d350a68691e7a5677bb61d05afe
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-05-06 17:49:57 -0400

    Initial backend commit

---
+++ Backend/cohere-analysis.js
@@ -0,0 +1,54 @@
+import cohere from "cohere-ai";
+import dotenv from "dotenv";
+dotenv.config();
+const cohere_key = process.env.COHERE_API_KEY;
+cohere.init(cohere_key);
+import healthJson from "./health-examples.json" assert { type: "json"};
+const healthExamples = JSON.parse(JSON.stringify(healthJson))
+import fillerJson from "./filler-examples.json" assert { type: "json"};
+const fillerExamples = JSON.parse(JSON.stringify(fillerJson));
+import tasteJson from "./taste-examples.json" assert { type: "json"};
+const tasteExamples = JSON.parse(JSON.stringify(tasteJson));
+
+export async function cohereGenerateDescription(ingredient) {
+    const response = await cohere.generate({
+        prompt: "The ingredient " + ingredient + " in food is used for",
+        max_tokens: 100
+    });
+    return response.body.generations[0].text.replace(/\s+/g,' ').trim();
+}
+
+export async function cohereSummarize(input) {
+    const response = await cohere.summarize({
+        text: input,
+    });
+    // console.log(response);
+    return response.body.summary;
+}
+
+export async function cohereClassifyPurpose(purpose, ingredientDescription) {
+    let sampleData;
+    switch (purpose){
+        case "health":
+            sampleData = healthExamples;
+            break;
+        case "filler":
+            sampleData = fillerExamples;
+            break;
+        case "taste":
+            sampleData = tasteExamples;
+            break;
+        // case "colour":
+        //     sampleData = colourExamples;
+        //     break;
+        default:
+            sampleData = healthExamples;
+            break;
+    }
+    ingredientDescription = [ingredientDescription];
+    const classification = await cohere.classify({
+        inputs: ingredientDescription,
+        examples: sampleData,
+    });
+    return classification.body.classifications[0];
+}
