44acb78f5ab48c27ee5b1b2408d781ec895baf43
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-05-07 03:28:26 -0400

    Added cohere reranking

--- Backend/subfunctions/cohere-analysis.js
+++ Backend/subfunctions/cohere-analysis.js
@@ -1,5 +1,6 @@
 import cohere from "cohere-ai";
 import dotenv from "dotenv";
+import axios from "axios";
 dotenv.config();
 const cohere_key = process.env.COHERE_API_KEY;
 cohere.init(cohere_key);
@@ -51,4 +52,22 @@ export async function cohereClassifyPurpose(purpose, ingredientDescription) {
         examples: sampleData,
     });
     return classification.body.classifications[0];
+}
+
+export async function cohereRerank(query, documents) {
+    const {data} = await axios.post('https://api.cohere.ai/v1/rerank', {
+        "return_documents": true,
+        "model": "rerank-english-v2.0",
+        "query": query,
+        "documents": documents,
+        "top_n": 3
+    }, {
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'authorization': 'Bearer ' + cohere_key
+        }
+    })
+    // console.log(data);
+    return data;
 }
